e7b9ecb620a9b95b7646f668f056135e4cf78bbd
edina-meszaros <dina.m@vipmail.hu>
2015-05-02 20:09:25 +0200

    Changed UI look and feel.

--- src/main/Spider.java
+++ src/main/Spider.java
@@ -2,6 +2,7 @@ package main;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 import controller.Controller;
 
@@ -9,17 +10,22 @@ public class Spider {
 
 	public static void main(String[] args) {
 
-		try {			
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
 		} catch (Exception e) {
-			// handle exception
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
 		}
-		
+
 		SwingUtilities.invokeLater(new Runnable() {
-            
+
 			@Override
             public void run() {
-                new Controller();                
+                new Controller();
             }
         });
 	}
